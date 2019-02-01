4da9612d7e63247b5ee6ec082f31ce5afe4c8b52
Shikanime Deva <deva.shikanime@protonmail.com>
2019-02-01 14:35:57 +0100

    fix volume nostale path for all

---
+++ run_nostale.sh
@@ -0,0 +1,24 @@
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    xhost + $(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+    docker run \
+        -it \
+        --env DISPLAY=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}'):0 \
+        --name nostale \
+        --privileged \
+        --rm \
+        --volume /tmp/.X11-unix/:/tmp/.X11-unix/ \
+        --volume "$HOME/.wine/drive_c/Program Files (x86)/Nostale":"/root/.wine/drive_c/Program Files (x86)/Nostale" \
+        nostale
+else
+    xhost +
+    docker run \
+        -it \
+        --env DISPLAY=$DISPLAY \
+        --network=host \
+        --name nostale \
+        --privileged \
+        --rm \
+        --volume /tmp/.X11-unix/:/tmp/.X11-unix/ \
+        --volume "$HOME/.wine/drive_c/Program Files (x86)/Nostale":"/root/.wine/drive_c/Program Files (x86)/Nostale" \
+        nostale
+fi
